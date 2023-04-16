18456f16e50e327c0883e503e2e82a0d7255406a
Louca Baignères <baigneres.louca@gmail.com>
2023-04-16 19:08:57 +0200

    🐛  Fix Delete Modal

--- js/delete.js
+++ js/delete.js
@@ -1,18 +1,20 @@
 // // Récupération du modal de suppression
 const deleteModal = document.querySelector(".overlay-delete");
 // // Récupération du bouton d'ouverture du modal de suppression
-const openDeleteModal = document.querySelector(".poubelle");
+const openDeleteModal = document.querySelectorAll(".poubelle");
 // // Récupération du bouton de fermeture du modal de suppression
 const closeDeleteModal = document.querySelector(".non");
 
-openDeleteModal.addEventListener("click", () => {
-    deleteModal.classList.remove("delete-hidden");
-});
+// openDeleteModal.addEventListener("click", () => {
+//     deleteModal.classList.remove("delete-hidden");
+// });
 
 closeDeleteModal.addEventListener("click", () => {
     deleteModal.classList.add("delete-hidden");
 });
 
-openDeleteModal.addEventListener("click", () => {
-    console.log("Salut");
+openDeleteModal.forEach((poubelle) => {
+    poubelle.addEventListener("click", () => {
+        deleteModal.classList.remove("delete-hidden");
+    });
 });
