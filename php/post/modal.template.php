4ffd5add6c0696c061105be08cc3dc9bfea31efd
Louca Baignères <baigneres.louca@gmail.com>
2023-04-16 19:12:21 +0200

    ✨ Add Filter Tag

--- php/post/modal.template.php
+++ php/post/modal.template.php
@@ -3,43 +3,23 @@
         <h2>Quelque chose à dire ?</h2>
         <div class="white-bar"></div>
         <div id="modal-tags" class="tags">
-            <button class="tag tag-1">
-                <p>Action</p>
-            </button>
-            <button class="tag tag-2">
-                <p>Animation</p>
-            </button>
-            <button class="tag tag-3">
-                <p>Aventure</p>
-            </button>
-            <button class="tag tag-4">
-                <p>Comédie</p>
-            </button>
-            <button class="tag tag-5">
-                <p>Drame</p>
-            </button>
-            <button class="tag tag-6">
-                <p>Fantastique</p>
-            </button>
-            <button class="tag tag-7">
-                <p>Horreur</p>
-            </button>
-            <button class="tag tag-8">
-                <p>Policier</p>
-            </button>
-            <button class="tag tag-9">
-                <p>Science-fiction</p>
-            </button>
-            <button class="tag tag-10">
-                <p>Old-school</p>   
-            </button>
+            <button class="tag tag-1">Action</button>
+            <button class="tag tag-2">Animation</button>
+            <button class="tag tag-3">Aventure</button>
+            <button class="tag tag-4">Comédie</button>
+            <button class="tag tag-5">Drame</button>
+            <button class="tag tag-6">Fantastique</button>
+            <button class="tag tag-7">Horreur</button>
+            <button class="tag tag-8">Policier</button>
+            <button class="tag tag-9">Science-fiction</button>
+            <button class="tag tag-10">Old-school</button>
         </div>
         <div class="whole-post">
             <form class="formPost" action="" method="POST" enctype="multipart/form-data">
                 <input type="hidden" name="form" value="formulaire_ajout_post">
                 <textarea placeholder="Écrivez ici..." name="post_content" id="post_content" class="posting" cols="30" rows="10" required></textarea>
-                <label for="file">Ajouter un média</label>
-                <input type="file" accept="image/png, image/jpeg, image/gif" class="upload-file">
+                <br>
+                <input name="post_pic" id="post_pic" type="file" accept="image/png, image/jpeg, image/gif" class="upload-file">
                 <input id="distribute" class="send_button" type="submit" value="Poster">
             </form>
             <!-- <div class="add-file">
--- php/post/posts.template.php
+++ php/post/posts.template.php
@@ -1,41 +1,19 @@
 <section class="accueil">
-    <div class="tag-list tags">
-        <button class="tag tag-0">
-            <p>Tout</p>
-        </button>
-        <button class="tag tag-1">
-            <p>Action</p>
-        </button>
-        <button class="tag tag-2">
-            <p>Animation</p>
-        </button>
-        <button class="tag tag-3">
-            <p>Aventure</p>
-        </button>
-        <button class="tag tag-4">
-            <p>Comédie</p>
-        </button>
-        <button class="tag tag-5">
-            <p>Drame</p>
-        </button>
-        <button class="tag tag-6">
-            <p>Fantastique</p>
-        </button>
-        <button class="tag tag-7">
-            <p>Horreur</p>
-        </button>
-        <button class="tag tag-8">
-            <p>Policier</p>
-        </button>
-        <button class="tag tag-9">
-            <p>Science-fiction</p>
-        </button>
-        <button class="tag tag-10">
-            <p>Old-school</p>   
-        </button>
+    <div class="tags">
+        <button class="tag tag-0">Tout</button>
+        <button class="tag tag-1">Action</button>
+        <button class="tag tag-2">Animation</button>
+        <button class="tag tag-3">Aventure</button>
+        <button class="tag tag-4">Comédie</button>
+        <button class="tag tag-5">Drame</button>
+        <button class="tag tag-6">Fantastique</button>
+        <button class="tag tag-7">Horreur</button>
+        <button class="tag tag-8">Policier</button>
+        <button class="tag tag-9">Science-fiction</button>
+        <button class="tag tag-10">Old-school</button>
     </div>
     <div class="all-publications">
-        <div class="publication">
+        <div class="publication tag-9">
             <div class="div-publication">
                 <img class="pp" src="./images/pp/PPLou.png" alt="🖼️" />
                 <div class="content-publication">
@@ -57,9 +35,33 @@
             </div>
             <div class="transition"></div>
         </div>
+        <div class="publication tag-9">
+            <div class="div-publication">
+                <img class="pp" src="./images/pp/PPLou.png" alt="🖼️" />
+                <div class="content-publication">
+                    <div class="id">
+                        <div class="name">
+                            <h2 class="username">Diamond</h2>
+                        </div>
+                        <div class="div-handle">
+                            <h3 class="handle">@diams</h3>
+                            <h3 class="dash">-</h3>
+                            <h3 class="time">16mn</h3>
+                        </div>
+                    </div>
+                    <div class="post">
+                        <p> Officiellement mon film favori de l'année ! </p>
+                        <img src="./images/img-post/mario.jpg" alt="🖼️" />
+                    </div>
+                </div>
+            </div>
+            <div class="transition"></div>
+        </div>
+    </div>
     <?php
     foreach ($posts as $post) {
         echo "<p>" . $post["post_content"] . "</p>";
+        // echo "<img" . $post["post_pic"] . "/>";
         echo date("d/m/Y", strtotime($post['post_date'])) .
             " à " . date("H:i", strtotime($post['post_date']));
     ?>
@@ -71,5 +73,4 @@
     <?php
     }
     ?>
-    </div>
 </section>
